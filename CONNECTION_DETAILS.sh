486dcad4187d3a93a08a7d3324ba8e09ede9981e
AxelDmt <jagertykah@gmail.com>
2024-04-03 11:05:43 +0200

    clean

--- CONNECTION_DETAILS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Provide your database details here ------------------------------------------
-export PGBIN=/etc/postgresql/14/main
-export PGHOST=localhost
-export PGPORT=5432
-export CITYDB=my3dcitydb
-export PGUSER=postgres
-#------------------------------------------------------------------------------
